844302d9634c55b4212b338b21bfe1c616626fbc
Sushil Verma <sushil.bit.cse@gmail.com>
2024-06-20 14:42:11 +0530

    Update projectController.java
    update api

--- project/src/main/java/com/sushil/project/controller/projectController.java
+++ project/src/main/java/com/sushil/project/controller/projectController.java
@@ -10,7 +10,7 @@ public class projectController {
 
     @GetMapping
     public String name(){
-        return "Hello Guys!! how are you!!  Hello internet";
+        return "Hello sushil! hello !!";
     }
 
     @GetMapping("/login")
