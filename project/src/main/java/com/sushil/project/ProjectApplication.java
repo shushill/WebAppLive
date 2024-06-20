6c09d64983ddd5046cfb00f83604fba5b82654f7
Sushil Verma <sushil.bit.cse@gmail.com>
2024-06-20 10:18:15 +0530

    made changes

--- project/src/main/java/com/sushil/project/ProjectApplication.java
+++ project/src/main/java/com/sushil/project/ProjectApplication.java
@@ -2,6 +2,7 @@ package com.sushil.project;
 
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import org.springframework.beans.factory.BeanDefinitionStoreException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -12,8 +13,6 @@ import java.util.logging.Logger;
 
 @SpringBootApplication
 public class ProjectApplication extends SpringBootServletInitializer {
-	private static final Logger logger = Logger.getLogger(ProjectApplication.class.getName());
-
 
 	public static void main(String[] args) {
 
@@ -24,10 +23,5 @@ public class ProjectApplication extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(ProjectApplication.class);
 	}
-//	@Override
-//	public void onStartup(Set<Class<?>> webAppInitializerClasses, ServletContext servletContext) throws ServletException {
-//		logger.info("Initializing servlet context...");
-//		super.onStartup(webAppInitializerClasses, servletContext);
-//		// Additional custom initialization can be added here
-//	}
+
 }
