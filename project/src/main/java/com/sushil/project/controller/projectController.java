1e8ea573b9be9842644d9041e666e7f70cacf88a
Sushil Verma <sushil.bit.cse@gmail.com>
2024-06-23 12:31:33 +0530

    add static and modified some files

--- project/src/main/java/com/sushil/project/controller/projectController.java
+++ project/src/main/java/com/sushil/project/controller/projectController.java
@@ -1,30 +1,27 @@
 package com.sushil.project.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api")
+@Controller
 public class projectController {
 
-    @GetMapping
-    public String name(){
-        return "Hello Ayushraj !!";
+    @GetMapping("/")
+    public String login(Model model) {
+        model.addAttribute("message", "Hello, Thymeleaf!");
+        return "login";
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "login now";
-    }
-
-    @GetMapping("/register")
-    public String register() {
-        return "register";
-    }
-
-    @GetMapping("/home")
-    public String home() {
-        return "home";
-    }
+//    @GetMapping("/register")
+//    public String register() {
+//        return "register";
+//    }
+//
+//    @GetMapping("/home")
+//    public String home() {
+//        return "home";
+//    }
 }
